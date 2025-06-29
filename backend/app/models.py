840d4a4ae15626fe9269029782762bd07099d143
Egor Gavrilov <hellguz@gmail.com>
2025-06-29 17:09:31 +0200

    readded all files

--- backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, HttpUrl
-
-class GeneratePromptRequest(BaseModel):
-    imageBase64: str # data:image/jpeg;base64,...
-
-class TransformImageRequest(BaseModel):
-    imageBase64: str
-    prompt: str
-
